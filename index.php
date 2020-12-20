b931f2346a9beb8ba16cc09b9492abc032222589
Aleksey Shapovalov <gigamel@yandex.ru>
2020-12-20 17:24:49 +0300

    Реализация метода createObjectByConstruct

--- index.php
+++ index.php
@@ -8,7 +8,15 @@ require '/var/www/iff/rusphp/src/PHP/Object/ObjectFactory.php';
 use ItForFree\rusphp\PHP\Object\ObjectClass\Constructor;
 use ItForFree\rusphp\PHP\Object\ObjectFactory;
 
-function debug($var = []) { echo '<pre>'; print_r($var); echo '</pre>'; }
+function debug($var = [], $dump = false, $die = false) {
+    echo '<pre>';
+    $dump === true ? var_dump($var) : print_r($var);
+    echo '</pre>';
+    
+    if ($die === true) {
+        die;
+    }
+}
 
 class Before
 {
@@ -17,13 +25,19 @@ class Before
 class Testing
 {
     public $before;
+    public $a;
+    public $b;
     
-    public function __construct(Before $before)
+    public function __construct(Before $before, int $b, int $a = 1)
     {
         $this->before = $before;
+        $this->a = $a;
+        $this->b = $b;
     }
 }
 
 $objF = ObjectFactory::createObjectByConstruct('Testing', [
+    3,
+    'b' => 4,
     'before' => new Before
 ]);
--- src/PHP/Object/ObjectFactory.php
+++ src/PHP/Object/ObjectFactory.php
@@ -1,53 +1,121 @@
 <?php
-
 namespace ItForFree\rusphp\PHP\Object;
 
 use ItForFree\rusphp\PHP\Object\ObjectClass\Constructor;
 
 /**
  * Для порождения объектов
- *
  */
 class ObjectFactory {
-   
-   /**
-    * Вернёт экземпляр класса -- или обычный объект или "одиночку" 
-    * (по паттерну Singletone)
-    * 
-    * @param string $className   Имя класса
-    * @param string $singletoneInstanceAccessStaticMethodName  необязательное имя статического метода для доступа к объекту-одиночке. По умолчанию 'get'
-    * @return object
-    */
-   public static function getInstanceOrSingletone($className, 
+
+    /**
+     * Вернёт экземпляр класса -- или обычный объект или "одиночку" 
+     * (по паттерну Singletone)
+     * 
+     * @param string $className   Имя класса
+     * @param string $singletoneInstanceAccessStaticMethodName  необязательное имя статического метода для доступа к объекту-одиночке. По умолчанию 'get'
+     * @return object
+     */
+    public static function getInstanceOrSingletone($className, 
         $singletoneInstanceAccessStaticMethodName = 'get')
-   {
-       $result = null;
-       if (Constructor::isPublic($className)) {
-          $result = new $className;
-       } else {
-            $result =  call_user_func($className . '::' 
+    {
+        $result = null;
+        if (Constructor::isPublic($className)) {
+            $result = new $className;
+        } else {
+            $result = call_user_func($className . '::' 
                 . $singletoneInstanceAccessStaticMethodName); 
-       }
+        }
        
-       return $result;
+        return $result;
+    }
+   
+    /**
+     * @param string $classname
+     * @param array $config
+     * @return null|object
+     */
+    public static function createObjectByConstruct(string $classname,
+        array $config = [])
+    {
+        $resultObject = null;
+        if (Constructor::isPublic($classname)) {
+            $configObject = [];
+            
+            $reflection = new \ReflectionClass($classname);
+            $constructor = $reflection->getConstructor();
+            $parameters = $constructor->getParameters();
+            
+            /**
+             * Собираем информацию о всех параметрах конструктора
+             * и их типах данных
+             */
+            $reflectionParameters = [];
+            foreach ($parameters as $parameter) {
+                $reflectionParameters[$parameter->getName()] = $parameter;
+            }
+
+            foreach ($reflectionParameters as $argument => $parameter) {
+                if (isset($config[$argument])) {
+                    $dependency = $config[$argument];
+                    unset($config[$argument]);
+                } else {
+                    foreach ($config as $key => $value) {
+                        if (!isset($reflectionParameters[$key])) {
+                            if (self::getType($value) == $parameter->getType()->getName()) {
+                                $dependency = $value;
+                                unset($config[$key]);
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if (!isset($dependency)) {
+                        try {
+                            $dependency = $parameter->getDefaultValue();
+                        } catch (\ReflectionException $e) {
+                            $dependency = null;
+                        }
+                    }
+                }
+                
+                $configObject[] = $dependency;
+                unset($dependency);
+            }
+
+            $resultObject = $reflection->newInstanceArgs($configObject);
+        }
+        
+        return $resultObject;
    }
    
    /**
-    * @param string $classname
-    * @param array $config
-    * @return null|object
+    * 
+    * @param any type $value
+    * @return string
     */
-   public static function createObjectByConstruct(string $classname,
-       array $config = [])
+   private static function getType($value = null)
    {
-       $resultObject = null;
-       if (Constructor::isPublic($classname)) {
-           
-           $resultObject = (new \ReflectionClass($classname))
-               ->newInstanceArgs($config);
-           
-           
+       if (is_object($value)) {
+           $type = get_class($value);
+       } else {
+           $type = gettype($value);
        }
-       return $resultObject;
+       
+       switch ($type) {
+           case 'integer':
+               $type = 'int';
+               break;
+           
+           case 'double':
+               $type = 'float';
+               break;
+           
+           case 'boolean':
+               $type = 'bool';
+               break;
+       }
+       
+       return $type;
    }
 }
